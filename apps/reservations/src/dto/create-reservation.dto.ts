512173eb9496f960a83f9c59a811c09299f12922
Dhananjaya Yapa <dhananjayayapa99@gmail.com>
2026-07-05 20:15:11 +0530

    fea: reservation management

---
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -0,0 +1,20 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
+  startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
+  endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  invoiceId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  placeId: string;
+}
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,15 @@
+import * as dns from 'node:dns';
 import { NestFactory } from '@nestjs/core';
-import { ReservationsModule } from './reservations.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+
+dns.setServers(['1.1.1.1', '1.0.0.1', '8.8.8.8']);
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
